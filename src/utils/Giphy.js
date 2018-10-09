7819daf650198de7a70162d6761106cfa8d12931
Christophe van Baalen <chichoch@gmail.com>
2018-10-09 23:04:51 +0200

    Started implemented some support for Giphy

---
+++ src/utils/Giphy.js
@@ -0,0 +1,11 @@
+const host = 'http://api.giphy.com';
+const randomEndpoint = '/v1/gifs/random';
+const apiKey = 'H098CFAR8lxoTJxEUKPwVdS0a97CTRCT';
+
+export default class Giphy {
+  getRandomGif(callback) {
+    fetch(host + randomEndpoint, {
+      
+    })
+  }
+}
