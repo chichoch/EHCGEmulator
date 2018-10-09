0f4ef70b59ce51274e675d9440197f6eccea9446
Christophe van Baalen <chichoch@gmail.com>
2018-10-09 23:05:41 +0200

    Added some info about Giphy

--- src/utils/Giphy.js
+++ src/utils/Giphy.js
@@ -2,10 +2,12 @@ const host = 'http://api.giphy.com';
 const randomEndpoint = '/v1/gifs/random';
 const apiKey = 'H098CFAR8lxoTJxEUKPwVdS0a97CTRCT';
 
+
+// TODO Implement this. Take a look at https://developers.giphy.com/docs/
 export default class Giphy {
   getRandomGif(callback) {
     fetch(host + randomEndpoint, {
-      
+
     })
   }
 }
